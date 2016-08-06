37822c2da00cff8cd6982d5e7b37c3d449592f13
MyatThuHeinn <myatthu.136@gmail.com>
2016-08-06 20:41:54 +0630

    hello world

---
+++ DestoryTheWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DestoryTheWorld
+//
+//  Created by Myat Thu Hein on 8/6/16.
+//  Copyright © 2016 Myat Thu Hein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print("adsf");
+        print("adsf");
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
